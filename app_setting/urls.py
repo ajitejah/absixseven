cc1ee8ebb162ab9cd7d15b001ca68e98d4aa6704
ajitejah <ajiteja.h@gmail.com>
2026-07-18 19:28:04 +0700

    setting 1

--- app_setting/urls.py
+++ app_setting/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     
     path('content/', views.content, name='setting_content'), 
     path('web/', views.web, name='setting_web'),
-    path('rule/', views.rule, name='setting_web'),
+    path('rule/', views.rule, name='setting_rule'),
     
     #path('question-update/', views.question_update, name='question_list'),  
     #path('node/assesment/delete', views.register_view, name='register'), 
