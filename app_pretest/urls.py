fa0f900c569feeb5b56139efeb648a4a621b5dcb
ajitejah <ajiteja.h@gmail.com>
2026-07-22 06:01:48 +0700

    UPDATE 20 JULY 2026 25

--- app_pretest/urls.py
+++ app_pretest/urls.py
@@ -6,9 +6,9 @@ app_name = 'app_pretest'
 urlpatterns = [ 
     
     path('lesson/', views.lesson, name='lesson'), 
-    path('lesson/create', views.lesson_create, name='lesson_create'),  
-    path('lesson/<int:lesson_id>/update', views.lesson_update, name='lesson_update'), 
-    path('lesson/<int:lesson_id>/delete', views.lesson_delete, name='lesson_delete'), 
+    path('lesson/create/', views.lesson_create, name='lesson_create'),  
+    path('lesson/<int:lesson_id>/update/', views.lesson_update, name='lesson_update'), 
+    path('lesson/<int:lesson_id>/delete/', views.lesson_delete, name='lesson_delete'), 
 
     path('', views.pretest, name='pretest'), 
 
