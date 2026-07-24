e1b5bf63cc1efee9c48923ab4ae93552964ed12e
ajitejah <ajiteja.h@gmail.com>
2026-07-24 22:15:41 +0700

    UPDATE 24 JULY 2026 11

--- absixseven/urls.py
+++ absixseven/urls.py
@@ -5,13 +5,11 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-
     path('reset-passwords/', views.reset_all_passwords, name='reset_passwords'),
 
     #-------------------------------------------#
-    #   >> Korpus Visitor
+    #   ▀▄▀▄ VISITOR
     #-------------------------------------------#
-     
     path('', views.home, name='home'),
     path('page/screening/', views.screening, name='screening'),
     path('admin/', views.admin_dashboard, name='admin_dashboard'), 
@@ -21,87 +19,52 @@ urlpatterns = [
     #path('quals/', views.quals, name='quals'), 
 
     #-------------------------------------------#
-    #   >> Korpus Auth
+    #   ▀▄▀▄ APP AUTH
     #-------------------------------------------# 
-
     path('auth/', include('app_auth.urls'), name='auth'), 
     path('admin/user/', include('app_auth.urls')), 
     
     
     #-------------------------------------------------------#
-    #   >> Screening
+    #   ▀▄▀▄ APP PRETEST
     #-------------------------------------------------------# 
-
     path('admin/pretest/', include(('app_pretest.urls', 'app_pretest'), namespace='admin_pretest')),
     path('teacher/pretest/', include(('app_pretest.urls', 'app_pretest'), namespace='teacher_pretest')),
- 
+    path('student/pretest/', include(('app_pretest.urls', 'app_pretest'), namespace='student_pretest')),
+      
     #-------------------------------------------------------#
-    #   >> APP_ROADMAP untuk diakses admin, teacher, student
+    #   ▀▄▀▄ APP ROADMAP
     #-------------------------------------------------------# 
-    path('admin/roadmap/',
-        include(('app_roadmap.urls', 'app_roadmap'), namespace='admin_roadmap')
-    ),
-
-    path('teacher/roadmap/',
-        include(('app_roadmap.urls', 'app_roadmap'), namespace='teacher_roadmap')
-    ),
-
-    path('student/roadmap/',
-        include(('app_roadmap.urls', 'app_roadmap'), namespace='student_roadmap')
-    ),
-
-    path('parent/roadmap/',
-        include(('app_roadmap.urls', 'app_roadmap'), namespace='parent_roadmap')
-    ),
+    path('admin/roadmap/', include(('app_roadmap.urls', 'app_roadmap'), namespace='admin_roadmap')),
+    path('teacher/roadmap/', include(('app_roadmap.urls', 'app_roadmap'), namespace='teacher_roadmap')),
+    path('student/roadmap/', include(('app_roadmap.urls', 'app_roadmap'), namespace='student_roadmap')),
+    path('parent/roadmap/', include(('app_roadmap.urls', 'app_roadmap'), namespace='parent_roadmap')),
 
     #-------------------------------------------------------#
-    #   >> APP_GUIDANCE untuk diakses teacher, student, parent
+    #   ▀▄▀▄ APP GUIDANCE
     #-------------------------------------------------------# 
-
-    path('admin/guidance/',
-        include(('app_guidance.urls', 'app_guidance'), namespace='admin_guidance')
-    ),
-
-    path('teacher/guidance/',
-        include(('app_guidance.urls', 'app_guidance'), namespace='teacher_guidance')
-    ), 
-     
-    path('parent/guidance/',
-        include(('app_guidance.urls', 'app_guidance'), namespace='parent_guidance')
-    ),
-
-    path('student/guidance/',
-        include(('app_guidance.urls', 'app_guidance'), namespace='student_guidance')
-    ),
+    path('admin/guidance/', include(('app_guidance.urls', 'app_guidance'), namespace='admin_guidance')),
+    path('teacher/guidance/', include(('app_guidance.urls', 'app_guidance'), namespace='teacher_guidance')), 
+    path('parent/guidance/', include(('app_guidance.urls', 'app_guidance'), namespace='parent_guidance')),
+    path('student/guidance/', include(('app_guidance.urls', 'app_guidance'), namespace='student_guidance')),
 
     #-------------------------------------------------------#
-    #   >> Scoring
+    #   ▀▄▀▄ APP SCORING
     #-------------------------------------------------------# 
-
-    path('admin/scoring/',
-        include(('app_scoring.urls', 'app_scoring'), namespace='admin_scoring')
-    ),
-
-    path('teacher/scoring/',
-        include(('app_scoring.urls', 'app_scoring'), namespace='teacher_scoring')
-    ),
-
-    path('student/scoring/',
-        include(('app_scoring.urls', 'app_scoring'), namespace='student_scoring')
-    ),
+    path('admin/scoring/', include(('app_scoring.urls', 'app_scoring'), namespace='admin_scoring')),
+    path('teacher/scoring/', include(('app_scoring.urls', 'app_scoring'), namespace='teacher_scoring')),
+    path('student/scoring/', include(('app_scoring.urls', 'app_scoring'), namespace='student_scoring')),
 
     #-------------------------------------------------------#
-    #   >> Screening
+    #   ▀▄▀▄ APP SCREENING
     #-------------------------------------------------------# 
-
     path('admin/screening/', include(('app_screening.urls', 'app_screening'), namespace='admin_screening')),
     path('teacher/screening/', include(('app_screening.urls', 'app_screening'), namespace='teacher_screening')),
     path('screening/', include(('app_screening.urls', 'app_screening'), namespace='visitor_screening')),
 
     #-------------------------------------------------------#
-    #   >> Setting
+    #   ▀▄▀▄ APP SETTING
     #-------------------------------------------------------# 
-
     path('admin/setting/', include(('app_setting.urls', 'app_setting'), namespace='admin_setting')),
 ]   
 
